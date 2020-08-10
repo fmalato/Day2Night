23aaad5eab05809c59f949974b0e793618506f70
federico <freaky1310@gmail.com>
2020-08-10 12:39:26 +0200

    Commit without images

---
+++ README.md
@@ -0,0 +1,7 @@
+
+# Day2Night: using CycleGAN to perform image translation from day to night and viceversa
+
+Heavily based on https://github.com/PramuPerera/In2I. Code has been edited just to match my use case.
+
+
+
---
+++ genDATA.py
@@ -0,0 +1,51 @@
+import glob
+from scipy.misc import imread
+from scipy.misc import imsave
+from scipy.misc import toimage
+from scipy.misc import imresize
+from scipy import ndimage
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy as sp
+
+import os
+paths = [x[0] for x in os.walk('/home/federico/Scrivania/KAIST/')]
+# Erasing all leaves
+paths = [x for x in paths if not x.endswith(('lwir', 'visible', 'labels'))]
+# Erasing parent folders
+paths = [x for x in paths if 'V0' in x]
+# Separating folders that contain daily images from the nightly ones
+day = ['set00', 'set01', 'set02', 'set06', 'set07', 'set08']
+for path in paths:
+    datasetA = path + '/visible/*.jpg'
+    datasetB = path + '/lwir/*.jpg'
+    # Choosing the right destination based on the current path
+    if any(folder in path for folder in day):
+        destination = 'datasets/Day2Night/day/'
+    else:
+        destination = 'datasets/Day2Night/night/'
+    print('Current path: %s    Destination: %s' % (path, destination))
+    dataA = glob.glob(datasetA)
+    dataA = sorted(dataA)
+    dataB = glob.glob(datasetB)
+    dataB = sorted(dataB)
+    frames = len(os.listdir(datasetA.split('*')[0]))
+    count = len(os.listdir(destination))
+    count_proc = 0
+    for i, j in zip(dataA, dataB):
+        count += 1
+        count_proc += 1
+        img_A = imread(j)
+        img_B = imread(i)
+        line = img_A
+        out = np.zeros((np.shape(img_B)[0], np.shape(img_B)[1] * 2, 3))
+        out[:, 0:np.shape(img_B)[1], 0] = img_B[:, :, 0]
+        out[:, 0:np.shape(img_B)[1], 1] = img_B[:, :, 1]
+        out[:, 0:np.shape(img_B)[1], 2] = img_B[:, :, 2]
+        out[:, np.shape(img_B)[1]:2 * np.shape(img_B)[1], 0] = line[:, :, 0]
+        out[:, np.shape(img_B)[1]:2 * np.shape(img_B)[1], 1] = line[:, :, 1]
+        out[:, np.shape(img_B)[1]:2 * np.shape(img_B)[1], 2] = line[:, :, 2]
+        toimage(out, cmin=0, cmax=255).save(destination + str(count) + '.jpg')
+        if count_proc % 100 == 0:
+            print('    Processed: %d/%d' % (count_proc, frames))
+    print('    Processed: %d/%d' % (count_proc, frames))
